cc40e1a27aa39f05ccd7a2cde39f2731e165df17
yuanchun103061251 <32770680+yuanchun103061251@users.noreply.github.com>
2017-10-30 15:19:05 +0800

    Add files via upload

--- MyHarrisCornerDetector.m
+++ MyHarrisCornerDetector.m
@@ -42,22 +42,29 @@ imagesc(frame);
 [xmax, ymax,ch] = size(I);
 xmin = 1;
 ymin = 1;
-
-
+%I0 = zeros(xmax+2,ymax+2);
+I0(1:xmax,1:ymax) = 0.299*I(:,:,1)+ 0.587*I(:,:,2) + 0.114*I(:,:,3);
 %%%%%%%%%%%%%%Intrest Points %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
+u=1;
+v=1;
 %%%%%%
 % get image gradient
-% [Your Code here] 
 % calculate Ix
 % calcualte Iy
+Ix = zeros(xmax,ymax);
+Iy = zeros(xmax,ymax);
+Ix = conv2(I0,dx,'same');
+Iy = conv2(I0,dy,'same');
 %%%%%
 % get all components of second moment matrix M = [[Ix2 Ixy];[Iyx Iy2]]; note Ix2 Ixy Iy2 are all Gaussian smoothed
 % [Your Code here] 
-% calculate Ix2  
+% calculate Ix2
+Ix2 =conv2(Ix.*Ix,g ,'same');
 % calculate Iy2
+Iy2 =conv2(Iy.*Iy,g ,'same');
 % calculate Ixy
+Ixy = conv2(Ix.*Iy,g ,'same');
+%Ixy = [Ix2*g,Ix*Iy'*g;Ix*Iy'*g,Iy2*g];
 %%%%%
 
 %% visualize Ixy
@@ -65,21 +72,36 @@ figure(2);
 imagesc(Ixy);
 
 %%%%%%% Demo Check Point -------------------
-
-
+%M = [Ix2,Ixy;Ixy,Iy2];
+for x=xmin:xmax-2
+    for y=ymin:ymax-2
+        %Ix(x,y) = (I0(x+u,y)-I0(x,y));%/255;
+        %Iy(x,y) = (I0(x,y+v)-I0(x,y));%/255;
+        %M(x,y) = [x,y]*[Ix2(x,y),Ixy(x,y);Ixy(x,y),Iy2(x,y)]*[x,y]';
+        M = [Ix2(x,y),Ixy(x,y);Ixy(x,y),Iy2(x,y)];
+        R(x,y) = det(M)-alpha*trace(M)^2; 
+        
+    end
+end
 %%%%%
 % get corner response function R = det(M)-alpha*trace(M)^2 
 % [Your Code here] 
+%R = det(M)-alpha*trace(M)^2; 
 % calculate R
 %%%%%
 
 %% make max R value to be 1000
 R=(1000/max(max(R)))*R; % be aware of if max(R) is 0 or not
+figure(3);
+imagesc(R);
 
 %%%%%
 %% using B = ordfilt2(A,order,domain) to complment a maxfilter
 sze = 2*r+1; % domain width 
 % [Your Code here] 
+domain = ones(sze,sze);
+order = 1;
+B = ordfilt2(R,order,domain);
 % calculate MX
 %%%%%
 
@@ -104,3 +126,13 @@ imagesc(uint8(I));
 hold on;
 plot(c1,r1,'or');
 return;
+%trash
+for x=xmin:xmax-2
+    for y=ymin:ymax-2
+        %Ix(x,y) = (I0(x+u,y)-I0(x,y));%/255;
+        %Iy(x,y) = (I0(x,y+v)-I0(x,y));%/255;
+        Ix(x,y) = sum(sum(I0(x:x+2,y:y+2).*dx));
+        Iy(x,y) = sum(sum(I0(x:x+2,y:y+2).*dy));
+    end
+end
+%}
